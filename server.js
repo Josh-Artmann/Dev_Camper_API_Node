f746cdeb2538e3bb51f765469d811ade84b7b3df
Josh-Artmann <60995733+Josh-Artmann@users.noreply.github.com>
2022-10-18 11:05:11 -0500

    Added all files

---
+++ server.js
@@ -0,0 +1,97 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const logger = require('./middleware/logger')
+const morgan = require('morgan')
+const colors = require('colors')
+const fileupload = require('express-fileupload')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const ratelimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
+
+//Load env vars
+dotenv.config({path: './config/config.env'})
+
+// Connect to Database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+const courses = require('./routes/courses')
+const auth = require('./routes/auth')
+const users = require('./routes/users')
+const reviews = require('./routes/reviews')
+
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+//Body Parser
+app.use(express.json())
+
+// cookie parser
+app.use(cookieParser())
+
+// Dev login middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+// file uploading
+app.use(fileupload())
+
+// sanitize data
+app.use(mongoSanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
+
+// Enable CORS
+app.use(cors())
+
+// Rate limiting
+const limiter = ratelimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+})
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp())
+
+// 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/courses', courses)
+app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
+app.use('/api/v1/reviews', reviews)
+
+
+// has to be after mounting routes or wont catch
+
+app.use(errorHandler)
+
+
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold))
+
+
+//handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) =>{
+    console.log(`Error: ${err.message}`.red)
+    //close server and exit process
+    server.close(() => process.exit(1))
+})
